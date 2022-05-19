2b81cb25a0c6d6cae61c9ec6cf6e2e7b81886fa7
Alejandro <consiglialejandro@gmail.com>
2022-05-19 15:39:38 +0200

    Removing api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     const consultaAPI = async () => {
-      const { data } = await axios.get("http://localhost:5000/api/hi", {
+      const { data } = await axios.get("/api/hi", {
         "Content-Type": "application/json",
       });
       setData(data);
