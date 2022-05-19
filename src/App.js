7a745ad4b4062f1f3b7bab555115c9daf18a94a1
Alejandro <consiglialejandro@gmail.com>
2022-05-19 12:35:53 +0200

    Uploading code

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const consultaAPI = async () => {
+      const { data } = await axios.get("http://localhost:5000/api/hi", {
+        "Content-Type": "application/json",
+      });
+      setData(data);
+    };
+
+    consultaAPI();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">{data}</header>
+    </div>
+  );
+}
+
+export default App;
